76c062a9ab8ee1158b6e31bf7a636f9d5a68b32a
Brunitop <68580588+Brunitop@users.noreply.github.com>
2024-04-26 13:13:08 -0700

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,94 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtNumRecords = new System.Windows.Forms.TextBox();
+            this.GenerateButton = new System.Windows.Forms.Button();
+            this.ExportButton = new System.Windows.Forms.Button();
+            this.textlabel = new System.Windows.Forms.Label();
+            this.tablaAlumnos = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tablaAlumnos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNumRecords
+            // 
+            this.txtNumRecords.Location = new System.Drawing.Point(49, 81);
+            this.txtNumRecords.Name = "txtNumRecords";
+            this.txtNumRecords.Size = new System.Drawing.Size(100, 20);
+            this.txtNumRecords.TabIndex = 0;
+            this.txtNumRecords.TextChanged += new System.EventHandler(this.txtNumRecords_TextChanged);
+            // 
+            // GenerateButton
+            // 
+            this.GenerateButton.Location = new System.Drawing.Point(49, 130);
+            this.GenerateButton.Name = "GenerateButton";
+            this.GenerateButton.Size = new System.Drawing.Size(75, 23);
+            this.GenerateButton.TabIndex = 1;
+            this.GenerateButton.Text = "Generar";
+            this.GenerateButton.UseVisualStyleBackColor = true;
+            this.GenerateButton.Click += new System.EventHandler(this.GenerateButton_Click);
+            // 
+            // ExportButton
+            // 
+            this.ExportButton.Location = new System.Drawing.Point(163, 130);
+            this.ExportButton.Name = "ExportButton";
+            this.ExportButton.Size = new System.Drawing.Size(75, 23);
+            this.ExportButton.TabIndex = 2;
+            this.ExportButton.Text = "Exportar";
+            this.ExportButton.UseVisualStyleBackColor = true;
+            this.ExportButton.Click += new System.EventHandler(this.ExportButton_Click);
+            // 
+            // textlabel
+            // 
+            this.textlabel.Location = new System.Drawing.Point(0, 0);
+            this.textlabel.Name = "textlabel";
+            this.textlabel.Size = new System.Drawing.Size(100, 23);
+            this.textlabel.TabIndex = 0;
+            // 
+            // tablaAlumnos
+            // 
+            this.tablaAlumnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tablaAlumnos.Location = new System.Drawing.Point(321, 24);
+            this.tablaAlumnos.Name = "tablaAlumnos";
+            this.tablaAlumnos.Size = new System.Drawing.Size(428, 150);
+            this.tablaAlumnos.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(155, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Numero de Registros";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tablaAlumnos);
+            this.Controls.Add(this.textlabel);
+            this.Controls.Add(this.ExportButton);
+            this.Controls.Add(this.GenerateButton);
+            this.Controls.Add(this.txtNumRecords);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tablaAlumnos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtNumRecords;
+        private System.Windows.Forms.Button GenerateButton;
+        private System.Windows.Forms.Button ExportButton;
+        private System.Windows.Forms.Label textlabel;
+        private System.Windows.Forms.DataGridView tablaAlumnos;
+        private System.Windows.Forms.Label label1;
     }
 }
-
